63a3a2ae525bcfae3a0ea4dafd1a45a3547ae0c2
Khalifouille <59296081+Khalifouille@users.noreply.github.com>
2025-02-09 20:25:12 +0100

    + de modifs mais soucis

--- script.js
+++ script.js
@@ -8,87 +8,84 @@ var priceArray = [
 ];
 
 var alreadyDid = [];
+const corsProxy = "https://cors-anywhere.herokuapp.com/";
 
-const getId = setInterval(() => {
-    var weapon = priceArray[~~(Math.random() * priceArray.length)];
-
-    fetch(`http://localhost:3000/api/${weapon.name}`, {
-            "headers": {
+async function fetchData() {
+    try {
+        const weapon = priceArray[~~(Math.random() * priceArray.length)];
+        const response = await fetch(`${corsProxy}http://localhost:3000/api/${weapon.name}`, {
+            headers: {
                 "accept": "application/json, text/plain, */*",
                 "accept-language": "en-US,en;q=0.9",
                 "sec-ch-ua": "\"Chromium\"",
                 "sec-ch-ua-mobile": "?0"
             },
-            "referrerPolicy": "no-referrer-when-downgrade",
-            "body": null,
-            "method": "GET",
-            "mode": "cors"
-        })
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(json) {
-            for (let value in json) {
-                const item = json[value];
-                fetch("https://gtalife/gameMenuAuctionEvent", {
-                    "headers": {
-                        "accept": "application/json, text/plain, */*",
-                        "content-type": "application/json;charset=UTF-8",
-                        "sec-ch-ua": "\"Chromium\"",
-                        "sec-ch-ua-mobile": "?0"
-                    },
-                    "referrer": "",
-                    "referrerPolicy": "no-referrer-when-downgrade",
-                    "body": JSON.stringify({
-                        "type": "takeOffer",
-                        "data": { "id": item.id, "sell": false }
-                    }),
-                    "method": "POST",
-                    "mode": "cors"
-                })
-                .then(() => {
-                    fetch(`https://api.gtaliferp.fr:8443/v1/extinction/marketplace/sell/${weapon.name}`)
-                        .then(marketResponse => marketResponse.json())
-                        .then(marketData => {
-                            if (marketData.length > 0) {
-                                const marketPrice = marketData[0].price;
-                                if (item.price !== marketPrice) {
-                                    const adjustedPrice = marketPrice - 1;
-                                    console.log(`${weapon.sanitized_name} : Prix ajusté du marché: ${adjustedPrice}`);
-                                    fetch("https://gtalife/gameMenuAuctionEvent", {
-                                        "headers": {
-                                            "accept": "application/json, text/plain, */*",
-                                            "content-type": "application/json;charset=UTF-8",
-                                            "sec-ch-ua": "\"Chromium\"",
-                                            "sec-ch-ua-mobile": "?0"
-                                        },
-                                        "referrer": "",
-                                        "referrerPolicy": "no-referrer-when-downgrade",
-                                        "body": JSON.stringify({
-                                            "type": "createOffer",
-                                            "data": {
-                                                "offer": {
-                                                    "itemId": weapon.name,
-                                                    "itemName": weapon.sanitized_name,
-                                                    "quantity": 1,
-                                                    "price": adjustedPrice
-                                                },
-                                                "sell": true
-                                            }
-                                        }),
-                                        "method": "POST",
-                                        "mode": "cors"
-                                    });
-                                }
-                            }
-                        })
-                        .catch(error => console.error("Erreur lors de la récupération des prix du marché :", error));
-                })
-                .catch(error => console.error("Erreur lors de la suppression de l'offre :", error));
+            method: "GET",
+            mode: "cors"
+        });
+
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP: ${response.status}`);
+        }
+
+        const json = await response.json();
+        for (let value in json) {
+            const item = json[value];
+            await fetch(`${corsProxy}https://gtalife/gameMenuAuctionEvent`, {
+                headers: {
+                    "accept": "application/json, text/plain, */*",
+                    "content-type": "application/json;charset=UTF-8",
+                    "sec-ch-ua": "\"Chromium\"",
+                    "sec-ch-ua-mobile": "?0"
+                },
+                body: JSON.stringify({
+                    "type": "takeOffer",
+                    "data": { "id": item.id, "sell": false }
+                }),
+                method: "POST",
+                mode: "cors"
+            });
+
+            const marketResponse = await fetch(`${corsProxy}https://api.gtaliferp.fr:8443/v1/extinction/marketplace/sell/${weapon.name}`);
+            if (!marketResponse.ok) {
+                throw new Error(`Erreur HTTP: ${marketResponse.status}`);
             }
-        })
-        .catch(error => console.error("Erreur lors de la récupération des objets :", error));
 
-}, 200);
+            const marketData = await marketResponse.json();
+            if (marketData.length > 0) {
+                const marketPrice = marketData[0].price;
+                if (item.price !== marketPrice) {
+                    const adjustedPrice = marketPrice - 1;
+                    console.log(`${weapon.sanitized_name} : Prix ajusté du marché: ${adjustedPrice}`);
+                    await fetch(`${corsProxy}https://gtalife/gameMenuAuctionEvent`, {
+                        headers: {
+                            "accept": "application/json, text/plain, */*",
+                            "content-type": "application/json;charset=UTF-8",
+                            "sec-ch-ua": "\"Chromium\"",
+                            "sec-ch-ua-mobile": "?0"
+                        },
+                        body: JSON.stringify({
+                            "type": "createOffer",
+                            "data": {
+                                "offer": {
+                                    "itemId": weapon.name,
+                                    "itemName": weapon.sanitized_name,
+                                    "quantity": 1,
+                                    "price": adjustedPrice
+                                },
+                                "sell": true
+                            }
+                        }),
+                        method: "POST",
+                        mode: "cors"
+                    });
+                }
+            }
+        }
+    } catch (error) {
+        console.error("Erreur :", error);
+    }
+}
 
-console.log("interval id = ", getId);
+const intervalId = setInterval(fetchData, 200);
+console.log("Interval ID =", intervalId);
