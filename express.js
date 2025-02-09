2541bb13295ae056320a319b5816b5a4728f19fc
Khalifouille <59296081+Khalifouille@users.noreply.github.com>
2025-02-09 16:44:41 +0100

    🔧 Fix ERR_REQUIRE_ESM for Chalk in Express.js
    Ce fork corrige l'erreur ERR_REQUIRE_ESM causée par l'importation de chalk en mode CommonJS. Comme chalk est désormais un module ESM, l'importation avec require() n'est plus supportée.

---
+++ express.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const port = 3000;
+const axios = require('axios');
+const proxies = fs.readFileSync('proxies.txt', 'utf-8').replace(/\r/gi, '').split('\n');
+
+import('chalk').then(chalk => {
+    process.on('uncaughtException', function (exception) {
+        console.log(exception);
+    });
+
+    // THIS BASICALLY JUST MAKES A REPLICA OF THE API SO YOU CAN USE IT WITHOUT ANY RATE-LIMITING
+    // LOCATED AT http://localhost:3000/
+    
+    app.get('/api/:item', async (req, res) => {
+        try {
+            console.log(`[${chalk.default.gray("PENDING")}] autobuy requested item : ${req.params.item}`);
+
+            const proxy_server = proxies[~~(Math.random() * proxies.length)];
+            const [ip, port] = proxy_server.split(':');
+
+            const response = await axios.get(`https://api.gtaliferp.fr:8443/v1/extinction/marketplace/sell/${req.params.item}`, {
+                proxy: { host: ip, port: parseInt(port, 10) }
+            });
+
+            console.log(`[${chalk.default.blue("SUCCESS")}] autobuy sent item : ${req.params.item} (${response.data.length} offers)`);
+            res.json(response.data);
+        } catch (err) {
+            console.log(`[${chalk.default.red("FAILURE")}] autobuy failed on : ${req.params.item}`);
+            res.status(500).json({ error: "Failed to fetch item" });
+        }
+    });
+
+    app.listen(port, () => {
+        console.log(`[+] autobuy server is up and running`);
+    });
+});
