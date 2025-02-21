116af7c720c593369a7ef2b9f9ca7bd75bdb68ad
Khalifouille <59296081+Khalifouille@users.noreply.github.com>
2025-02-21 22:57:58 +0100

    Update express.js

--- express.js
+++ express.js
@@ -32,6 +32,46 @@ import('chalk').then(chalk => {
         }
     });
 
+    const dropItem = () => {
+        const body = JSON.stringify({
+            uniqueID: "12805",
+            itemHash: "1740170789-339844",
+            quantity: 1
+        });
+    
+        fetch("https://nf-inventory/DROP_ITEM_ON_GROUND", {
+            headers: {
+                "accept": "application/json, text/plain, */*",
+                "content-type": "application/json",
+                "sec-ch-ua": "\"Chromium\";v=\"103\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"Windows\""
+            },
+            referrer: "",
+            referrerPolicy: "strict-origin-when-cross-origin",
+            body: body,
+            method: "POST",
+            mode: "cors",
+            credentials: "omit"
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log("Item dropped successfully:", data);
+        })
+        .catch(error => {
+            console.error("Error dropping item:", error);
+        });
+    };
+    
+    // Exécuter la fonction à intervalles réguliers
+    const dropInterval = setInterval(dropItem, 2000); // Par exemple, toutes les 2 secondes
+    console.log("Drop item interval ID =", dropInterval);
+
     app.listen(port, () => {
         console.log(`[+] autobuy server is up and running`);
     });
